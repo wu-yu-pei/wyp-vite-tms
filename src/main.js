c83c508c4046eaa009d527ea9da2c7908104f4b9
wuyupei <495174699@qq.com>
2022-03-06 14:13:28 +0800

    1

--- src/main.js
+++ src/main.js
@@ -9,17 +9,24 @@ import App from './App.vue'
 function bootStrap() {
   const app = createApp(App)
 
+  // 初始化Store
   setupStore(app)
 
+  // 初始化图标
   setupIcon(app)
 
+  // 初始化Router
   setupRouter(app)
 
+  // 初始化自定义指令
   setupDirective(app)
 
+  // 初始化全局属性(过滤器)
   setupGlobalProp(app)
 
+  // 挂载app
   app.mount('#app')
 }
 
+// 启动项目
 bootStrap()
